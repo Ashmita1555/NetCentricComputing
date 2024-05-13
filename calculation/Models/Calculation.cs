96c398ba2963682b2123ba382aa29df263b35657
SAAGAR <38255285+Sagar-Timalsena@users.noreply.github.com>
2024-05-13 16:27:08 +0545

    calculator

---
+++ calculation/Models/Calculation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace calculation.Models
+{
+    public class Calculation
+    {
+
+        [Required(ErrorMessage = "Please enter the first number")]
+        public double n1 { get; set; }
+
+        [Required(ErrorMessage = "Please enter the second number")]
+        public double n2 { get; set; }
+
+        public double result { get; set; }
+    }
+}
